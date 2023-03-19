397d1582de94985e9c86041f86dc9a39f127f527
Richard Tichý <tichy@artify.zone>
2023-03-19 16:15:16 +0100

    Implement solution

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,10 @@
+// solution for https://codeforces.com/contest/1807/problem/B
 fun main() {
-    println("Hello World!")
+    (1..readln().toInt()).forEach { _ ->
+        readln().toInt()
+        val a = readln().split(" ").map { it.toInt() }
+        val sumEven = a.filter { it % 2 == 0 }.sum()
+        val sumOdd = a.filter { it % 2 == 1 }.sum()
+        println(if(sumOdd < sumEven) "YES" else "NO")
+    }
 }
